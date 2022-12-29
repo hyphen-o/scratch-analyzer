b62fdd858ee09510ac6f35a0f4b0c65f51f62adb
hyphen-o <otekapu@gmail.com>
2022-12-29 14:18:33 +0900

    first commit

---
+++ InputSnapTest.py
@@ -0,0 +1,181 @@
+import os
+import time
+import sys
+import math
+import subprocess
+import pandas as pd
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from concurrent.futures import ThreadPoolExecutor
+
+# ids = pd.read_csv("/Users/socsel/dataset.csv", usecols=["p_ID"], dtype="str")
+# retryIds = pd.read_csv("/Users/socsel/retryIds.csv", usecols=["p_ID"], dtype="str")
+retryIds = pd.read_csv("out_csv/retryIds.csv",
+                       usecols=["p_ID"], dtype="str")
+retryIds = list(retryIds)
+
+# 座標情報を格納しているキー名
+MOVE = ['DX', 'DY']
+SET = ['X', 'Y']
+DEGREE = ['DEGREES']
+STEP = ['STEPS']
+# 待機時間情報を格納しているキー名
+WAIT = ['DURATION', 'SECS']
+
+
+# 作品のASTを取得する
+id = "747365086"
+
+program_x = 0
+program_y = 0
+
+df_none = pd.read_csv('out_csv/SortedScripts[0].csv')
+for i in range(len(df_none.index)):
+    print(df_none.iloc[i][1])
+    print(df_none.iloc[i][2])
+
+
+chrome_service = fs.Service("/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=chrome_service)
+
+driver.get("https://scratch.mit.edu/projects/" + str(id) + "/editor")
+
+wait = WebDriverWait(driver, 15)
+loadClassName = "loader_bottom-block_1-3rO"
+
+try:
+    wait.until(EC.presence_of_element_located(
+        (By.CLASS_NAME, loadClassName)))  # 作品のロード開始を待機
+    wait.until_not(EC.presence_of_element_located(
+        (By.CLASS_NAME, loadClassName)))  # 作品のロード完了を待機
+except TimeoutException as te:
+    print(str(id) + ": timeout")  # 作品が存在していないため，スキップ
+    driver.close()
+    sys.exit()
+
+try:
+    # スプライトのX座標,Y座標を取ってくる
+    positionX = driver.find_elements(
+        By.XPATH, '/html/body/div[1]/div/div[3]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/label/input')
+    for i in positionX:
+        print(i.get_attribute("value"))
+    positionY = driver.find_elements(
+        By.XPATH, '/html/body/div[1]/div/div[3]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[3]/label/input')
+    for i in positionY:
+        print(i.get_attribute("value"))
+
+    program_x = 0
+    program_y = 0
+
+    start = driver.find_elements(
+        By.CLASS_NAME, "green-flag_green-flag_1kiAo")
+
+    start[0].click()
+    print('click')
+
+except Exception as e:
+    print(str(id) + ": crash error")  # Scratchがクラッシュするエラー
+    print(str(e))
+    retryIds.append(str(id))  # エラーが発生した場合，後ほど再試行するためidを配列に追加して保持
+    driver.close()
+    sys.exit()
+
+savePath = "/Users/keigo-o/Desktop/screenshots/" + str(id)
+if not os.path.isdir(savePath):
+    os.mkdir(savePath)
+
+startTime = time.time()
+
+
+def ScreenShot():
+    for i in range(100):
+        time.sleep(0.2)
+
+        try:
+            end = driver.find_elements(
+                By.CSS_SELECTOR, ".green-flag_green-flag_1kiAo.green-flag_is-active_2oExT")
+            if len(end) == 0:
+                break  # 作品のプログラムが終了していれば，スクリーンショット収集終了
+            png = driver.find_element(
+                By.CLASS_NAME, "stage-wrapper_stage-canvas-wrapper_3ewmd").screenshot_as_png  # スクリーンショットを取得
+        except Exception as e:
+            print(str(id) + ": error")  # 予期せぬエラー
+            print(e)
+            retryIds.append(str(id))  # エラーが発生した場合，後ほど再試行するためidを配列に追加して保持
+            driver.close()
+            break
+
+        try:
+            with open(savePath + "/" + str(id) + "-" + str(i) + ".png", "wb") as f:
+                f.write(png)
+            print("save png: " + str(i))
+        except OSError as oe:
+            print("save error")  # 画像保存失敗エラー
+            driver.close()
+            continue
+    driver.close()
+
+
+def getMovement():
+    # if (not df_none.iloc[index][2] == None):
+    #     json_data = json.loads(df_none.iloc[index][2])
+    #     for k, v in json_data.items():
+    #         print(k + '!!!!!')
+    #         print(v)
+    #     if (df_none.iloc[index][2] == 'DX' or 'DY'):
+    #         X += df_none.iloc
+
+    if (not df_none.iloc[index][1] == None):
+        return df_none.iloc[index][1]
+
+
+def AutoInput():
+    X = 0
+    Y = 0
+    for i in range(100000):
+
+        try:
+            end = driver.find_elements(
+                By.CSS_SELECTOR, ".green-flag_green-flag_1kiAo.green-flag_is-active_2oExT")
+            if len(end) == 0:
+                break  # 作品のプログラムが終了していれば，スクリーンショット収集終了
+
+            # result = getMovement()
+
+            # スプライトのX座標Y座標を取得
+            for j in positionX:
+                print('X: ' + j.get_attribute("value"))
+            for j in positionY:
+                print('Y: ' + j.get_attribute("value"))
+
+            Inputbox = driver.find_elements(
+                By.XPATH, '/html/body/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[1]/canvas')
+            print(Inputbox)
+        except Exception as e:
+            print(str(id) + ": error")  # 予期せぬエラー
+            print(e)
+            driver.close()
+            break
+
+
+index = 0
+with ThreadPoolExecutor(max_workers=2) as executor:
+    executor.submit(ScreenShot)
+    executor.submit(AutoInput)
+
+elapsedTime = time.time() - startTime
+print(str(id) + ": " + str(elapsedTime) + "s")
+
+df = pd.DataFrame(retryIds, columns=["p_ID"])
+
+if len(retryIds) > 1:
+    df.to_csv("retryIds.csv")
+
+driver.close()
+driver.quit()
---
+++ scratch_code_to_astTest.py
@@ -0,0 +1,188 @@
+'''
+ブロック構造をASTに変換する
+（引数ブロックなどへは未対応）
+'''
+import json
+import pandas as pd
+import requests
+import sys
+
+
+class CodeToASTNode():
+
+    c_blocks = [
+        'control_repeat', 'control_forever', 'control_if', 'control_if_else',
+        'control_repeat_until', 'control_all_at_once', 'control_while', 'control_for_each'
+    ]
+
+    def __init__(self, json_blocks):
+        self.node_id = 0
+        self.json_blocks = json_blocks
+        self.df_script = pd.DataFrame(
+            [['SCRIPT', 0, -1]], columns=['block', 'node_id', 'parent_node_id'])  # 記録用のデータフレーム
+
+    def get_result(self):
+        return self.df_script
+
+    def add_df(self, block, node_id, parent_node_id):
+        se_add = pd.Series([block, node_id, parent_node_id],
+                           index=self.df_script.columns)
+        self.df_script = self.df_script.append(se_add, ignore_index=True)
+
+    def depth_first_search(self, node_hash, parent_node_id):
+        '''
+        深さ優先探索でノードを調べる
+        '''
+        self.node_id += 1
+        block = self.json_blocks[node_hash]['opcode']  # ブロック名
+
+        # 記録用のデータフレームにノード情報を追加する
+
+        # 座標移動ブロックの場合に座標も出力する
+        if block == 'motion_gotoxy':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['X'][1][1]) + " " + \
+                str(self.json_blocks[node_hash]['inputs']['Y'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'motion_movesteps':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['STEPS'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'motion_glidesecstoxy':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['X'][1][1]) + " " + \
+                str(self.json_blocks[node_hash]['inputs']['Y'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'motion_changexby':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['DX'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'motion_setx':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['X'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'motion_changeyby':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['DY'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'motion_sety':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['inputs']['Y'][1][1])
+            self.add_df(block, self.node_id, parent_node_id)
+        # 入力ブロックの場合に入力の種類も出力する
+        elif block == 'sensing_keyoptions':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['fields']['KEY_OPTION'][0])
+            self.add_df(block, self.node_id, parent_node_id)
+        elif block == 'event_whenkeypressed':
+            block += ' ' + \
+                str(self.json_blocks[node_hash]['fields']['KEY_OPTION'][0])
+            self.add_df(block, self.node_id, parent_node_id)
+        else:
+            self.add_df(block, self.node_id, parent_node_id)
+
+        # Cブロックについての処理
+        if block in self.c_blocks:
+            # if-elseブロックは2段階構造のため，処理を分ける
+            if block == 'control_if_else':
+                current_node_id = self.node_id
+                for key in ['SUBSTACK', 'SUBSTACK2']:
+                    self.node_id += 1
+                    self.add_df('SUBSTACK', self.node_id, current_node_id)
+                    if key in self.json_blocks[node_hash]['inputs']:
+                        self.depth_first_search(
+                            self.json_blocks[node_hash]['inputs'][key][1], self.node_id)
+
+            else:
+                if 'SUBSTACK' in self.json_blocks[node_hash]['inputs']:
+                    self.depth_first_search(
+                        self.json_blocks[node_hash]['inputs']['SUBSTACK'][1], self.node_id)
+
+        if self.json_blocks[node_hash]['next'] == None:
+            return
+        else:
+            self.depth_first_search(
+                self.json_blocks[node_hash]['next'], parent_node_id)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == '-r':
+        if len(sys.argv) < 2:
+            print(
+                '''
+                作品IDと書き込み先のファイルパスを指定してください
+                例. $python3 scratch_code_to_ast.py -r 619086872 ./out.csv
+                '''
+            )
+        else:
+            project_id = sys.argv[2]
+            write_path = sys.argv[3]  # 書き出し用のファイルパス
+
+            # url = requests.get(
+            #     f'https://api.scratch.mit.edu/projects/{project_id}')
+            # project_token = json.loads(url.text)['project_token']
+            # jsontext = requests.get(
+            #     f'https://projects.scratch.mit.edu/{project_id}?token={project_token}').text
+
+            try:
+                json_data = json.loads(jsontext)
+            except json.JSONDecodeError as e:
+                print(e)
+                print('jsonデータの取得に失敗')
+                exit()
+
+            if 'code' in json_data.keys():
+                if json_data['code'] == 'NotFound':
+                    print('jsonデータの中身が空です')
+                    exit()
+    elif sys.argv[1] == '-f':
+        if len(sys.argv) < 2:
+            print(
+                '''
+                読み込むjsonファイルパスと書き込み先のファイルパス，project_idを指定してください
+                例. $python3 scratch_code_to_ast.py -r ./project.json ./out.csv 8392890483
+                '''
+            )
+        else:
+            read_path = sys.argv[2]
+            write_path = sys.argv[3]
+            project_id = sys.argv[4]
+
+            try:
+                json_data = json.load(open(read_path))
+            except json.JSONDecodeError as e:
+                print('jsonデータの取得に失敗')
+                exit()
+
+    if 'code' in json_data.keys():
+        if json_data['code'] == 'NotFound':
+            print('jsonデータの中身が空です')
+            exit()
+
+    try:
+        list_df = []
+        script_id = 0
+        for sprite_data in json_data['targets']:
+            sprite_name = sprite_data['name']
+            json_blocks = sprite_data['blocks']
+            for k, v in json_blocks.items():
+                cta = CodeToASTNode(json_blocks=json_blocks)
+                cta.depth_first_search(node_hash=k, parent_node_id=0)
+                script_df = cta.get_result()
+                script_df['script_id'] = script_id
+                script_df['sprite_name'] = sprite_name
+                list_df.append(script_df)
+                script_id += 1
+
+        # 結果の出力
+        df_concat = pd.concat(list_df)
+        df_concat['project_id'] = project_id
+        df_concat.to_csv(write_path, index=False)
+    except:
+        print('ID:{}の解析中にエラー発生'.format(project_id))
+
+        import traceback
+        traceback.print_exc()
+
+
+# self.json_blocks[node_hash]['inputs']['DX'][1][1]
