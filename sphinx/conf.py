9c4cfb54d743a8387ad4945b7f406df74436ceeb
hyphen-o <otekapu@gmail.com>
2023-11-04 01:33:25 +0900

    add sphinx

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -1,12 +1,3 @@
-import os
-import sys
-
-# ルートディレクトリをパスに追加
-# この操作にて、ルートディレクトリ直下にある、〇〇.pyが読み込まれます。
-sys.path.extend([
-    os.path.abspath('../..'),           # ルートディレクトリ
-])
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -18,17 +9,16 @@ sys.path.extend([
 project = 'scratcher'
 copyright = '2023, keigo-o'
 author = 'keigo-o'
-release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+language = 'ja'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -1,5 +1,5 @@
 .. scratcher documentation master file, created by
-   sphinx-quickstart on Mon Oct 30 16:54:27 2023.
+   sphinx-quickstart on Sat Nov  4 01:26:20 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
