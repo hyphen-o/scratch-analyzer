a64469632136e340455807ff1f3942a4b684f2e2
hyphen-o <otekapu@gmail.com>
2023-11-04 03:10:12 +0900

    update docs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -13,8 +13,6 @@ author = 'keigo-o'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
@@ -23,5 +21,5 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_pdj_theme'
 html_static_path = ['_static']
