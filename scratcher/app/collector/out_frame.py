b9867031f9fb67e0f38470d9e2e33dce465da094
keigo-0314 <okamoto.keigo@g.wakayama-u.jp>
2023-11-17 15:32:16 +0900

    format

--- scratcher/app/collector/out_frame.py
+++ scratcher/app/collector/out_frame.py
@@ -4,12 +4,12 @@ import random
 import scipy.stats as stats
 import os
 
-TRACK_CSV = '../../out/tracked/'
+TRACK_CSV = "../../out/tracked/"
 
 li = []
 for file_name in os.listdir(TRACK_CSV):
-  df = pd.read_csv(TRACK_CSV + file_name)
-  li.append(len(df.index))
+    df = pd.read_csv(TRACK_CSV + file_name)
+    li.append(len(df.index))
 
 # # 中央値を計算
 q1 = np.percentile(li, 25)
@@ -18,5 +18,3 @@ median = np.median(li)
 print(q1)
 print(median)
 print(q3)
-
-
