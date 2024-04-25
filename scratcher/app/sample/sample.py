e497cce7b089c5df6eceb350cc57923fb5e58193
momoka-h <momo5588.school@gmail.com>
2024-04-25 11:23:15 +0900

    ブロック数とブロック，CTスコア取得追記

--- scratcher/app/sample/sample.py
+++ scratcher/app/sample/sample.py
@@ -28,46 +28,34 @@ sample_id4 = 2767515787
 #     with open('data/test_PM_' + str(sample_id2) + '.json', 'w') as f:
 #         json.dump(str(PM), f, indent=2)
 
+# print("metaData: " + str(MD))
+# jsonファイルに出力
+# with open('data/test_MD_' + str(sample_id2) + '.json', 'w') as f:
+#     json.dump(str(MD), f, indent=2)
 
-# とりあえず何個取ってくるか後に設定する
-# for id in range(276751787, 277751787):
-    # MD = scratch_client.get_meta(id)
-
-MD = scratch_client.get_meta(sample_id2)
-
-# プロジェクトのメタ情報をとってくる
+# リミックスしていたら1個前のプロジェクトのID出力
 # parentが1個前
-if MD:
-    # print("metaData: " + str(MD))
-    # jsonファイルに出力
-    # with open('data/test_MD_' + str(sample_id2) + '.json', 'w') as f:
-    #     json.dump(str(MD), f, indent=2)
+# if MD["remix"]["parent"]:
+#     # print("parent_id: " + str(MD["remix"]["parent"]))
+#     with open('data/test_MD_' + str(sample_id2) + '.json', 'w') as f:
+#         json.dump(str(MD["remix"]["parent"]), f, indent=2)
     
-    # リミックスしていたら1個前のプロジェクトのID出力
-    # if MD["remix"]["parent"]:
-    #     # print("parent_id: " + str(MD["remix"]["parent"]))
-    #     with open('data/test_MD_' + str(sample_id2) + '.json', 'w') as f:
-    #         json.dump(str(MD["remix"]["parent"]), f, indent=2)   
-        # リミックスだった場合プロジェクトのjsonファイルの取得（リミックス前と後）
-        
-    # ブロック数を取得
-    project_manager = ProjectManager(sample_id4)
-    blocks_length = project_manager.get_blocks_length()
-    # project = prjman.ProjectManager(sample_id)
-    # blocks = project.prjman.get_blocks_length()
-    # ブロックを取得
-    #blockType = scratch_client.get_blocks(sample_id2)
-    # スプライト数を取得
-    # CTスコアを取得
-    #CTscore = drscratch_analyzer.total_score(sample_id2)
-    #CTscore = drscratch_analyzer.analyze(sample_id2)
-    # 出力
-    print("blocks count =" + blocks_length)
-    #print("blockType =" + blockType)
-    #print("CTscore = " + CTscore)
+# IDを取得
 
+# ブロック数を取得
+project_manager = ProjectManager(sample_id4)
+blocks_length = project_manager.get_blocks_length()
+# ブロックを取得
+blockType = project_manager.get_blocks()
+# スプライト数を取得 "isStage"の数がスプライト数？1つはステージなので-1する
 
-    #test
-    #print(drscratch_analyzer.ct_score(sample_id2))
+# CTスコアを取得
+mastery = drscratch_analyzer.Mastery(sample_id4)
+CTscore = mastery.total_score()
+#CTscore = drscratch_analyzer.analyze(sample_id2)
 
-# リミックスだった場合のプロジェクトのCTScoreの取得（リミックス前と後）
+# 出力
+print("blocks count =" + blocks_length)
+print("blockType =" + blockType)
+print("CTscore = " + CTscore)
+
