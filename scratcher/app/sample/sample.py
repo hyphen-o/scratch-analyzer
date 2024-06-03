3b5cdafa7b0ffa12cd2872ea3c408af47b08b306
momoka-h <momo5588.school@gmail.com>
2024-06-03 10:00:37 +0900

    id取得完了

--- scratcher/app/sample/sample.py
+++ scratcher/app/sample/sample.py
@@ -11,52 +11,90 @@ from api import drscratch_analyzer
 import prjman
 from prjman import ProjectManager
 
-# # サンプルプロジェクト
-sample_id = 797975999
+# # 指定ディレクトリ内のjsonファイルからidを参照するモジュール
+def extract_ids(obj, ids, parent_key=None):
+    """
+    JSONオブジェクトを再帰的に探索し、'id'キーの値を抽出してリストに追加する。
+    'author'キーの下にある'id'は除外する。
+    """
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if key == 'id' and 'author' not in obj:
+                if value is not None:  # Noneのチェックを追加
+                    ids.append(value)
+            elif isinstance(value, (dict, list)):
+                extract_ids(value, ids)
+    elif isinstance(obj, list):
+        for item in obj:
+            extract_ids(item, ids)
 
-# # ブロック数を取得
-project_manager = ProjectManager(sample_id)
-# print("instance")
-blocks_length = project_manager.get_all_blocks_length()
+def extract_ids_from_json_files(directory):
+    """
+    指定ディレクトリ内のすべてのJSONファイルからIDを抽出してリストに追加する。
+    """
+    all_ids = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            filepath = os.path.join(directory, filename)
+            with open(filepath, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                extract_ids(data, all_ids)
+    return all_ids
 
-# # ブロックを取得
-blockType_length = project_manager.get_blocks_type_length()
 
-# # スプライト数を取得 "isStage"の数がスプライト数？1つはステージなので-1する
-sprits_length = project_manager.get_sprites_length()
+# 使用例
+directory = '../../dataset/projects'
+all_ids = extract_ids_from_json_files(directory)
+print(all_ids)
+print(len(all_ids))
 
-# # CTスコア合計点数を取得
-mastery = drscratch_analyzer.Mastery()
-mastery.process("../../sample_json/797975999.json")
-mastery.analyze("./out.json")
 
-with open('out.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
-# # CTScoreの値をint型の変数に格納
-ct_score = data["CTScore"]
+# # # サンプルプロジェクト
+# sample_id = 797975999
 
-# # 出力
-print("blocks count = " + str(blocks_length))
-print("blockType =" + str(blockType_length))
-print("sprites count = " + str(sprits_length))
-print("CTscore = " + str(ct_score))
+# # # ブロック数を取得
+# project_manager = ProjectManager(id)
+# # print("instance")
+# blocks_length = project_manager.get_all_blocks_length()
 
-# # csvに保存
+# # # ブロックを取得
+# blockType_length = project_manager.get_blocks_type_length()
 
-data = [
-    [blocks_length, blockType_length, sprits_length, ct_score]
-]
+# # # スプライト数を取得 "isStage"の数がスプライト数？1つはステージなので-1する
+# sprits_length = project_manager.get_sprites_length()
 
-# ヘッダー
-header = ["blocks_length", "blockType_length", "sprits_length", "CTscore"]
+# # # CTスコア合計点数を取得
+# mastery = drscratch_analyzer.Mastery()
+# mastery.process("../../sample_json/797975999.json")
+# mastery.analyze("./out.json")
 
-with open('example.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
+# with open('out.json', 'r', encoding='utf-8') as file:
+#     data = json.load(file)
+# # # CTScoreの値をint型の変数に格納
+# ct_score = data["CTScore"]
 
-    writer.writerow(header)
-    writer.writerows(data)
+# # # 出力
+# print("blocks count = " + str(blocks_length))
+# print("blockType =" + str(blockType_length))
+# print("sprites count = " + str(sprits_length))
+# print("CTscore = " + str(ct_score))
 
-print("csv data update")
+# # # csvに保存
+
+# data = [
+#     [blocks_length, blockType_length, sprits_length, ct_score]
+# ]
+
+# # ヘッダー
+# header = ["blocks_length", "blockType_length", "sprits_length", "CTscore"]
+
+# with open('example.csv', mode='w', newline='', encoding='utf-8') as file:
+#     writer = csv.writer(file)
+
+#     writer.writerow(header)
+#     writer.writerows(data)
+
+# print("csv data update")
 
 
 
